5ed3663eafc2338e7c1189a712aae2df3984b511
Scott Price <scottprice7@gmail.com>
2016-08-01 20:10:53 -0700

    init2

---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+public class PaddleMovement : MonoBehaviour
+{
+
+    public int playerId = 0;
+    public float moveSpeed = 3f;
+
+    private Player player;
+    private Vector3 moveVector2;
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(playerId);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetInput();
+        ProcessInput();
+
+	
+	}
+
+    private void GetInput()
+    {
+        moveVector2.y = player.GetAxis("Move Vertical");
+        
+    }
+
+    private void ProcessInput()
+    {
+        transform.Translate(moveVector2 * moveSpeed *Time.deltaTime);  
+       // if(moveVector2.y != 0.0f)
+            
+            
+        
+    }
+}
