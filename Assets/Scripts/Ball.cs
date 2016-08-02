db7e84b2d45d051ad91bd7bdb27161118643712e
devastadus <scottprice7@gmail.com>
2016-08-01 21:17:04 -0700

    making ball bounce

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();
+	
+		rb2D.AddForce(new Vector2(100,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(rb2D.velocity);
+	
+	}
+}
